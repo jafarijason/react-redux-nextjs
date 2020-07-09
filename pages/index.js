fa36a137879f85ba88f9cbcba12e4fac6786ef34
Jason <jason.jafari.m@gmail.com>
2020-07-08 23:31:44 -0400

    cahange the index.js to statefull component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
+import React from 'react'
 import './index.css'
 import Card from './Card';
+import data from '../data/data.json'
+
+export default class Index extends React.Component {
+  static async getInitialProps() {
+    return { cards: data }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src="/static/logo.png"
+            className="static-logo"
+            alt="logo"
+          />
+        </header>
+        <div className="Grid">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    )
+  }
+
+}
 
 
-export default () => (
-  <div className="App">
-    <header className="App-header">
-      <img 
-      src="/static/logo.png" 
-      className="static-logo"
-      alt="logo"
-      />
-    </header>
-    <div className="Grid">
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  </div>
-);
