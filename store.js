9b8eea525bcfdc553cd58e901e27a2e109d17939
Jason <jason.jafari.m@gmail.com>
2020-07-09 00:01:19 -0400

    Add redux reducers

--- store.js
+++ store.js
@@ -19,12 +19,29 @@ export const initialCards = () => {
   };
 };
 
-export const addItem = (item) =>{
-  return{
-    type:'ADD',
-    item
+export const addItem = (item) => {
+  return {
+    type: "ADD",
+    item,
+  };
+};
+
+// reducers
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INITIALCARDS":
+      return {
+        cards: action.cards,
+      };
+    case "ADD":
+      return {
+        ...state,
+        cards: [...cards, action.item],
+      };
+    default:
+      return state;
   }
-}
+};
 
 // create store
 export const initStore = (initialState = startState) => {
